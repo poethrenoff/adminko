4362098625b13a5ac95f605db686dc379367fc21
Константинов Дмитрий <poethrenoff@gmail.com>
2013-05-27 21:34:13 +0400

    field_pk + todo

---
+++ htdocs/include/field/pk.php
@@ -0,0 +1,18 @@
+<?php
+class field_pk extends field
+{
+    public function get($content)
+    {
+        ///
+    }
+    
+    public function form($content)
+    {
+        ///
+    }
+    
+    public function set($content)
+    {
+        ///
+    }
+}
