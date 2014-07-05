c47a0818b4c5188c6d4f64ae26422ac417794e8e
Константинов Дмитрий <poethrenoff@gmail.com>
2014-07-05 13:55:27 +0400

    Форматирование

--- htdocs/include/Captcha.php
+++ htdocs/include/Captcha.php
@@ -4,10 +4,10 @@ namespace Adminko;
 /**
  * SimpleCaptcha class
  */
-class Captcha {
-
+class Captcha
+{
     /** Width of the image */
-    public $width  = 180;
+    public $width = 180;
 
     /** Height of the image */
     public $height = 50;
@@ -33,7 +33,7 @@ class Captcha {
     public $colors = array(
         array(27, 78, 181), // blue
         array(22, 163, 35), // green
-        array(214, 36, 7),  // red
+        array(214, 36, 7), // red
     );
 
     /** Shadow color in RGB-array or false */
@@ -48,16 +48,19 @@ class Captcha {
      * - maxSize: max font size
      */
     public $fonts = array(
-        'comic'  => array('spacing' =>4,'minSize' => 20, 'maxSize' => 30, 'font' => 'comic.ttf'),
-        'courbi' => array('spacing' =>2,'minSize' => 20, 'maxSize' => 26, 'font' => 'courbi.ttf'),
-        'arial'  => array('spacing' =>4,'minSize' => 20, 'maxSize' => 34, 'font' => 'arial.ttf'),
-        'times'  => array('spacing' =>2,'minSize' => 20, 'maxSize' => 34, 'font' => 'times.ttf'),
+        'comic' => array('spacing' => 4, 'minSize' => 20, 'maxSize' => 30, 'font' => 'comic.ttf'),
+        'courbi' => array('spacing' => 2, 'minSize' => 20, 'maxSize' => 26, 'font' => 'courbi.ttf'),
+        'arial' => array('spacing' => 4, 'minSize' => 20, 'maxSize' => 34, 'font' => 'arial.ttf'),
+        'times' => array('spacing' => 2, 'minSize' => 20, 'maxSize' => 34, 'font' => 'times.ttf'),
     );
 
     /** Wave configuracion in X and Y axes */
-    public $Yperiod    = 10;
+    public $Yperiod = 10;
+
     public $Yamplitude = 1;
-    public $Xperiod    = 10;
+
+    public $Xperiod = 10;
+
     public $Xamplitude = 1;
 
     /** letter rotation clockwise */
@@ -69,7 +72,7 @@ class Captcha {
      */
     public $scale = 3;
 
-    /** 
+    /**
      * Blur effect for better image quality (but slower image processing).
      * Better image results with scale=3
      */
@@ -77,10 +80,9 @@ class Captcha {
 
     /** Debug? */
     public $debug = false;
-    
+
     /** Image format: jpeg or png */
     public $imageFormat = 'png';
-
 
     /** GD image */
     public $im;
@@ -91,7 +93,8 @@ class Captcha {
     /**
      * Draw captcha image
      */
-    public static function create() {
+    public static function create()
+    {
         $captcha = new Captcha();
         $captcha->CreateImage();
     }
@@ -99,9 +102,10 @@ class Captcha {
     /**
      * Check captcha value
      */
-    public static function check($captcha_value) {
+    public static function check($captcha_value)
+    {
         $result = isset($_SESSION[self::$session_var]) &&
-            !strcasecmp($captcha_value, $_SESSION[self::$session_var]);
+                !strcasecmp($captcha_value, $_SESSION[self::$session_var]);
         unset($_SESSION[self::$session_var]);
         return $result;
     }
@@ -109,16 +113,17 @@ class Captcha {
     /**
      * Creates the image
      */
-    public function CreateImage() {
+    public function CreateImage()
+    {
         $ini = microtime(true);
 
         /** Initialization */
         $this->ImageAllocate();
-        
-        /** Text insertion */
-        $text     = $this->GetCaptchaText();
 
-        $fontcfg  = $this->fonts[array_rand($this->fonts)];
+        /** Text insertion */
+        $text = $this->GetCaptchaText();
+
+        $fontcfg = $this->fonts[array_rand($this->fonts)];
         $this->WriteText($text, $fontcfg);
 
         $_SESSION[self::$session_var] = $text;
@@ -132,9 +137,7 @@ class Captcha {
 
 
         if ($this->debug) {
-            imagestring($this->im, 1, 1, $this->height-8,
-                "$text {$fontcfg['font']} ".round((microtime(true)-$ini)*1000)."ms",
-                $this->GdFgColor
+            imagestring($this->im, 1, 1, $this->height - 8, "$text {$fontcfg['font']} " . round((microtime(true) - $ini) * 1000) . "ms", $this->GdFgColor
             );
         }
 
@@ -146,32 +149,27 @@ class Captcha {
     /**
      * Creates the image resources
      */
-    protected function ImageAllocate() {
+    protected function ImageAllocate()
+    {
         // Cleanup
         if (!empty($this->im)) {
             imagedestroy($this->im);
         }
 
-        $this->im = imagecreatetruecolor($this->width*$this->scale, $this->height*$this->scale);
-        
+        $this->im = imagecreatetruecolor($this->width * $this->scale, $this->height * $this->scale);
+
         // Background color
-        $this->GdBgColor = imagecolorallocate($this->im,
-            $this->backgroundColor[0],
-            $this->backgroundColor[1],
-            $this->backgroundColor[2]
+        $this->GdBgColor = imagecolorallocate($this->im, $this->backgroundColor[0], $this->backgroundColor[1], $this->backgroundColor[2]
         );
-        imagefilledrectangle($this->im, 0, 0, $this->width*$this->scale, $this->height*$this->scale, $this->GdBgColor);
+        imagefilledrectangle($this->im, 0, 0, $this->width * $this->scale, $this->height * $this->scale, $this->GdBgColor);
 
         // Foreground color
-        $color           = $this->colors[mt_rand(0, sizeof($this->colors)-1)];
+        $color = $this->colors[mt_rand(0, sizeof($this->colors) - 1)];
         $this->GdFgColor = imagecolorallocate($this->im, $color[0], $color[1], $color[2]);
 
         // Shadow color
         if (!empty($this->shadowColor)) {
-            $this->GdShadowColor = imagecolorallocate($this->im,
-                $this->shadowColor[0],
-                $this->shadowColor[1],
-                $this->shadowColor[2]
+            $this->GdShadowColor = imagecolorallocate($this->im, $this->shadowColor[0], $this->shadowColor[1], $this->shadowColor[2]
             );
         }
     }
@@ -181,17 +179,18 @@ class Captcha {
      *
      * @return string Text
      */
-    protected function GetCaptchaText($length = null) {
+    protected function GetCaptchaText($length = null)
+    {
         if (empty($length)) {
             $length = rand($this->minWordLength, $this->maxWordLength);
         }
 
-        $words  = "abcdefghijlmnopqrstvwyz";
+        $words = "abcdefghijlmnopqrstvwyz";
         $vocals = "aeiou";
 
-        $text  = "";
+        $text = "";
         $vocal = rand(0, 1);
-        for ($i=0; $i<$length; $i++) {
+        for ($i = 0; $i < $length; $i++) {
             if ($vocal) {
                 $text .= substr($vocals, mt_rand(0, 4), 1);
             } else {
@@ -202,84 +201,80 @@ class Captcha {
         return $text;
     }
 
-   /**
+    /**
      * Text insertion
      */
-    protected function WriteText($text, $fontcfg = array()) {
+    protected function WriteText($text, $fontcfg = array())
+    {
         if (empty($fontcfg)) {
             // Select the font configuration
-            $fontcfg  = $this->fonts[array_rand($this->fonts)];
+            $fontcfg = $this->fonts[array_rand($this->fonts)];
         }
-        $fontfile = APP_DIR.'/image/fonts/'.$fontcfg['font'];
+        $fontfile = APP_DIR . '/image/fonts/' . $fontcfg['font'];
 
         /** Increase font-size for shortest words: 9% for each glyp missing */
-        $lettersMissing = $this->maxWordLength-strlen($text);
-        $fontSizefactor = 1+($lettersMissing*0.09);
-        
-        $bbox = imageftbbox($fontcfg['maxSize']*$this->scale*$fontSizefactor, 0, $fontfile, $text);
+        $lettersMissing = $this->maxWordLength - strlen($text);
+        $fontSizefactor = 1 + ($lettersMissing * 0.09);
+
+        $bbox = imageftbbox($fontcfg['maxSize'] * $this->scale * $fontSizefactor, 0, $fontfile, $text);
         $text_width = $bbox[2] - $bbox[0];
-        
+
         // Text generation (char by char)
-        $x      = round(($this->width*$this->scale - $text_width)/2);
-        $y      = round(($this->height*27/40)*$this->scale);
+        $x = round(($this->width * $this->scale - $text_width) / 2);
+        $y = round(($this->height * 27 / 40) * $this->scale);
         $length = strlen($text);
-        for ($i=0; $i<$length; $i++) {
-            $degree   = rand($this->maxRotation*-1, $this->maxRotation);
-            $fontsize = rand($fontcfg['minSize'], $fontcfg['maxSize'])*$this->scale*$fontSizefactor;
-            $letter   = substr($text, $i, 1);
-            
+        for ($i = 0; $i < $length; $i++) {
+            $degree = rand($this->maxRotation * -1, $this->maxRotation);
+            $fontsize = rand($fontcfg['minSize'], $fontcfg['maxSize']) * $this->scale * $fontSizefactor;
+            $letter = substr($text, $i, 1);
+
             if ($this->shadowColor) {
-                $coords = imagettftext($this->im, $fontsize, $degree,
-                    $x+$this->scale, $y+$this->scale,
-                    $this->GdShadowColor, $fontfile, $letter);
+                $coords = imagettftext($this->im, $fontsize, $degree, $x + $this->scale, $y + $this->scale, $this->GdShadowColor, $fontfile, $letter);
             }
-            $coords = imagettftext($this->im, $fontsize, $degree,
-                $x, $y,
-                $this->GdFgColor, $fontfile, $letter);
-            $x += ($coords[2]-$x) + ($fontcfg['spacing']*$this->scale);
+            $coords = imagettftext($this->im, $fontsize, $degree, $x, $y, $this->GdFgColor, $fontfile, $letter);
+            $x += ($coords[2] - $x) + ($fontcfg['spacing'] * $this->scale);
         }
     }
+
     /**
      * Wave filter
      */
-    protected function WaveImage() {
+    protected function WaveImage()
+    {
         // X-axis wave generation
-        $xp = $this->scale*$this->Xperiod*rand(1,3);
+        $xp = $this->scale * $this->Xperiod * rand(1, 3);
         $k = rand(0, 100);
-        for ($i = 0; $i < ($this->width*$this->scale); $i++) {
-            imagecopy($this->im, $this->im,
-                $i-1, sin($k+$i/$xp) * ($this->scale*$this->Xamplitude),
-                $i, 0, 1, $this->height*$this->scale);
+        for ($i = 0; $i < ($this->width * $this->scale); $i++) {
+            imagecopy($this->im, $this->im, $i - 1, sin($k + $i / $xp) * ($this->scale * $this->Xamplitude), $i, 0, 1, $this->height * $this->scale);
         }
 
         // Y-axis wave generation
         $k = rand(0, 100);
-        $yp = $this->scale*$this->Yperiod*rand(1,2);
-        for ($i = 0; $i < ($this->height*$this->scale); $i++) {
-            imagecopy($this->im, $this->im,
-                sin($k+$i/$yp) * ($this->scale*$this->Yamplitude), $i-1,
-                0, $i, $this->width*$this->scale, 1);
+        $yp = $this->scale * $this->Yperiod * rand(1, 2);
+        for ($i = 0; $i < ($this->height * $this->scale); $i++) {
+            imagecopy($this->im, $this->im, sin($k + $i / $yp) * ($this->scale * $this->Yamplitude), $i - 1, 0, $i, $this->width * $this->scale, 1);
         }
     }
+
     /**
      * Reduce the image to the final size
      */
-    protected function ReduceImage() {
+    protected function ReduceImage()
+    {
         // Reduzco el tama�o de la imagen
         $imResampled = imagecreatetruecolor($this->width, $this->height);
 
-        imagecopyresampled($imResampled, $this->im,
-            0, 0, 0, 0,
-            $this->width, $this->height,
-            $this->width*$this->scale, $this->height*$this->scale
+        imagecopyresampled($imResampled, $this->im, 0, 0, 0, 0, $this->width, $this->height, $this->width * $this->scale, $this->height * $this->scale
         );
         imagedestroy($this->im);
         $this->im = $imResampled;
     }
+
     /**
      * File generation
      */
-    protected function WriteImage() {
+    protected function WriteImage()
+    {
         if ($this->imageFormat == 'png') {
             header("Content-type: image/png");
             ob_clean();
@@ -290,12 +285,12 @@ class Captcha {
             imagejpeg($this->im, null, 90);
         }
     }
+
     /**
      * Cleanup
      */
-    protected function Cleanup() {
+    protected function Cleanup()
+    {
         imagedestroy($this->im);
     }
 }
-
-
