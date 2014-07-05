53863b5fc62a74839e6dca1d0b1df2040d10e671
Константинов Дмитрий <poethrenoff@gmail.com>
2014-07-05 12:01:41 +0400

    + тесты

--- test/Tests/ModelTest.php
+++ test/Tests/ModelTest.php
@@ -1,10 +1,12 @@
 <?php
+use Adminko\Model\Model;
+
 class ModelTest extends PHPUnit_Framework_TestCase
 {
 	public function testCreate()
 	{
-		$model = model::factory('text');
-		$this->assertInstanceOf('model', $model);
-		$this->assertInstanceOf('model_text', $model);
+		$model = Model::factory('text');
+		$this->assertInstanceOf('Adminko\Model\Model', $model);
+		$this->assertInstanceOf('Adminko\Model\TextModel', $model);
 	}
 }
